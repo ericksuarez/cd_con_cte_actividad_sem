24a991c6373816760215065648c994d45b7a8817
ericksuarez <erick.suarez.buendia@gmail.com>
2022-05-30 15:18:51 -0500

    Cambio a CD por Negocio
    Se reemplazaron las tablas de paso por las tablas crystal de cada negocio

--- script/10_activos_perdidos_general_prev.sql
+++ script/10_activos_perdidos_general_prev.sql
@@ -12,34 +12,34 @@ WITH
 	PIVOTE AS ( -- Obtenemos un pivote de clientes para evitar algunos casos de duplicados (que no deberían pasar)
 
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_cap_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_cap_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_capnom_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_capnom_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_cre_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_cre_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_tor_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_tor_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_rem_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_rem_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_dex_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_dex_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_div_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_div_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_pre_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_pre_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_pgs_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_pgs_sem WHERE num_periodo_sem = ${num_periodo_sem}
 UNION
 SELECT id_master
-FROM ${esquema_cu}.cu_con_cte_actividad_afr_sem WHERE num_periodo_sem = ${num_periodo_sem}
+FROM ${esquema_cd}.cd_con_cte_actividad_afr_sem WHERE num_periodo_sem = ${num_periodo_sem}
 )
 , ACT AS  ( -- Obtenemos las marcas de actividad/inactividad/perdidos y cliente/usuario
     SELECT pvt.id_master
@@ -342,125 +342,58 @@ FROM ${esquema_cu}.cu_con_cte_actividad_afr_sem WHERE num_periodo_sem = ${num_pe
                             ,COALESCE(ind_perdido_div, 0)
                             ,COALESCE(ind_perdido_pgs, 0)) = 1 THEN 'USUARIO'
                ELSE 'OTRO' END AS cod_cte_usua_sin_afr
- FROM PIVOTE pvt      
- LEFT JOIN -- Obtenemos las marcas de Captación
-	(
-	SELECT id_master
-		,ind_activo_cap
-		,ind_inactivo_cap
-		,ind_perdido_cap
-		,cod_estatus_semanal_cap
-		,cod_estatus_52s_cap
-	FROM ${esquema_cu}.cu_con_cte_actividad_cap_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS cap ON pvt.id_master = cap.id_master
+    FROM PIVOTE AS pvt
+
+    LEFT JOIN -- Obtenemos las marcas de Captación
+(SELECT id_master, ind_activo_cap, ind_inactivo_cap, ind_perdido_cap, cod_estatus_semanal_cap, cod_estatus_52s_cap
+ FROM ${esquema_cd}.cd_con_cte_actividad_cap_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS cap
+ON pvt.id_master = cap.id_master
+
 LEFT JOIN -- Obtenemos las marcas de Captación Nómina
-	(
-	SELECT id_master
-		,ind_activo_capnom
-		,ind_inactivo_capnom
-		,ind_perdido_capnom
-		,cod_estatus_semanal_capnom
-		,cod_estatus_52s_capnom
-	FROM ${esquema_cu}.cu_con_cte_actividad_capnom_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS capnom ON pvt.id_master = capnom.id_master
+(SELECT id_master, ind_activo_capnom, ind_inactivo_capnom, ind_perdido_capnom, cod_estatus_semanal_capnom, cod_estatus_52s_capnom
+ FROM ${esquema_cd}.cd_con_cte_actividad_capnom_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS capnom
+ON pvt.id_master = capnom.id_master
+
 LEFT JOIN -- Obtenemos las marcas de Crédito
-	(
-	SELECT id_master
-		,ind_activo_credimax
-		,ind_activo_nom
-		,ind_activo_tor
-		,ind_activo_cre
-		,ind_inactivo_credimax
-		,ind_inactivo_nom
-		,ind_inactivo_tor
-		,ind_inactivo_cre
-		,ind_perdido_credimax
-		,ind_perdido_nom
-		,ind_perdido_tor
-		,ind_perdido_cre
-		,num_sem_sld_0_credimax
-		,num_sem_sld_0_nom
-		,num_sem_sld_0_tor
-		,num_sem_sld_0_cre
-		,cod_estatus_semanal_credimax
-		,cod_estatus_semanal_nom
-		,cod_estatus_semanal_tor
-		,cod_estatus_semanal_cre
-		,cod_estatus_52s_credimax
-		,cod_estatus_52s_nom
-		,cod_estatus_52s_tor
-		,cod_estatus_52s_cre
-	FROM ${esquema_cu}.cu_con_cte_actividad_cre_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS cre ON pvt.id_master = cre.id_master
+(SELECT id_master, ind_activo_credimax, ind_activo_nom, ind_activo_tor, ind_activo_cre
+       ,ind_inactivo_credimax, ind_inactivo_nom, ind_inactivo_tor, ind_inactivo_cre
+       ,ind_perdido_credimax, ind_perdido_nom, ind_perdido_tor, ind_perdido_cre
+       ,num_sem_sld_0_credimax, num_sem_sld_0_nom, num_sem_sld_0_tor, num_sem_sld_0_cre
+       ,cod_estatus_semanal_credimax, cod_estatus_semanal_nom, cod_estatus_semanal_tor, cod_estatus_semanal_cre
+       ,cod_estatus_52s_credimax, cod_estatus_52s_nom, cod_estatus_52s_tor, cod_estatus_52s_cre
+ FROM ${esquema_cd}.cd_con_cte_actividad_cre_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS cre
+ON pvt.id_master = cre.id_master
+
 LEFT JOIN -- Obtenemos las marcas de Remesas
-	(
-	SELECT id_master
-		,ind_activo_rem
-		,ind_perdido_rem
-		,num_sem_inact AS num_sem_inact_rem
-		,cod_estatus_semanal_rem
-		,cod_estatus_52s_rem
-	FROM ${esquema_cu}.cu_con_cte_actividad_rem_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS rem ON pvt.id_master = rem.id_master
+(SELECT id_master ,ind_activo_rem, ind_perdido_rem, num_sem_inact AS num_sem_inact_rem, cod_estatus_semanal_rem, cod_estatus_52s_rem
+ FROM ${esquema_cd}.cd_con_cte_actividad_rem_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS rem
+ON pvt.id_master = rem.id_master
+
 LEFT JOIN -- Obtenemos las marcas de DEX
-	(
-	SELECT id_master
-		,ind_activo_dex
-		,ind_perdido_dex
-		,num_sem_inact AS num_sem_inact_dex
-		,cod_estatus_semanal_dex
-		,cod_estatus_52s_dex
-	FROM ${esquema_cu}.cu_con_cte_actividad_dex_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS dex ON pvt.id_master = dex.id_master
+(SELECT id_master ,ind_activo_dex, ind_perdido_dex, num_sem_inact AS num_sem_inact_dex, cod_estatus_semanal_dex, cod_estatus_52s_dex
+ FROM ${esquema_cd}.cd_con_cte_actividad_dex_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS dex
+ON pvt.id_master = dex.id_master
+
 LEFT JOIN -- Obtenemos las marcas de Divisas
-	(
-	SELECT id_master
-		,ind_activo_div
-		,ind_perdido_div
-		,num_sem_inact AS num_sem_inact_div
-		,cod_estatus_semanal_div
-		,cod_estatus_52s_div
-	FROM ${esquema_cu}.cu_con_cte_actividad_div_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS dvs ON pvt.id_master = dvs.id_master
+(SELECT id_master, ind_activo_div, ind_perdido_div, num_sem_inact AS num_sem_inact_div, cod_estatus_semanal_div, cod_estatus_52s_div
+ FROM ${esquema_cd}.cd_con_cte_actividad_div_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS dvs
+ON pvt.id_master = dvs.id_master
+
 LEFT JOIN -- Obtenemos las marcas de Prendario
-	(
-	SELECT id_master
-		,ind_activo_pre
-		,ind_perdido_pre
-		,num_sem_inact AS num_sem_inact_pre
-		,cod_estatus_semanal_pre
-		,cod_estatus_52s_pre
-	FROM ${esquema_cu}.cu_con_cte_actividad_pre_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS pre ON pvt.id_master = pre.id_master
+(SELECT id_master, ind_activo_pre, ind_perdido_pre, num_sem_inact AS num_sem_inact_pre, cod_estatus_semanal_pre, cod_estatus_52s_pre
+ FROM ${esquema_cd}.cd_con_cte_actividad_pre_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS pre
+ON pvt.id_master = pre.id_master
+
 LEFT JOIN -- Obtenemos las marcas de PGS
-	(
-	SELECT id_master
-		,ind_activo_pgs
-		,ind_perdido_pgs
-		,num_sem_inact AS num_sem_inact_pgs
-		,cod_estatus_semanal_pgs
-		,cod_estatus_52s_pgs
-	FROM ${esquema_cu}.cu_con_cte_actividad_pgs_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS pgs ON pvt.id_master = pgs.id_master
+(SELECT id_master, ind_activo_pgs, ind_perdido_pgs, num_sem_inact AS num_sem_inact_pgs, cod_estatus_semanal_pgs, cod_estatus_52s_pgs
+ FROM ${esquema_cd}.cd_con_cte_actividad_pgs_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS pgs
+ON pvt.id_master = pgs.id_master
+
 LEFT JOIN -- Obtenemos las marcas de Afore
-	(
-	SELECT id_master
-		,ind_activo_afr
-		,ind_perdido_afr
-		,num_sem_inact AS num_sem_inact_afr
-		,cod_estatus_semanal_afr
-		,cod_estatus_52s_afr
-	FROM ${esquema_cu}.cu_con_cte_actividad_afr_sem
-	WHERE num_periodo_sem = ${num_periodo_sem}
-	) AS afr ON pvt.id_master = afr.id_master
+(SELECT id_master, ind_activo_afr, ind_perdido_afr, num_sem_inact AS num_sem_inact_afr, cod_estatus_semanal_afr, cod_estatus_52s_afr
+ FROM ${esquema_cd}.cd_con_cte_actividad_afr_sem WHERE num_periodo_sem = ${num_periodo_sem}) AS afr
+ON pvt.id_master = afr.id_master
+
 )
 SELECT id_master
       ,ind_activo_cap
